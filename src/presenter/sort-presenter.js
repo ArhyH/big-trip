9fbc351ff5660981311f04574f57b2272d5ecfb0
Nick Horbach <creat.photo.art@gmail.com>
2026-02-24 17:54:55 +0200

    [M1-T3] Update SortView

--- src/presenter/sort-presenter.js
+++ src/presenter/sort-presenter.js
@@ -1,21 +1,13 @@
 import { SORTS } from '../common/consts';
 import { render } from '../render';
-
-import SortView from '../view/sort/sort-view';
-import SortItemView from '../view/sort/sort-item-view';
+import SortView from '../view/sort';
 
 export default class SortPresenter {
   constructor(contentNode) {
     this.contentNode = contentNode;
   }
 
-  sort = new SortView();
-
   init() {
-    render(this.sort, this.contentNode);
-
-    SORTS.forEach((sort) => {
-      render(new SortItemView(sort), this.sort.getElement());
-    });
+    render(new SortView(SORTS), this.contentNode);
   }
 }
